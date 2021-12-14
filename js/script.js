33ed6de97cdcb87c35232a98645a0a26b4b0786c
ArdanHerdiansyah <58681058+ArdanHerdiansyah@users.noreply.github.com>
2021-12-14 16:54:34 +0700

    update code

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ function searchMovie() {
         type: 'get',
         dataType: 'json',
         data: {
-            'apikey' : 'eed1e4a1',
+            'apikey': 'eed1e4a1',
             's': $('#search-input').val()
         },
         success: function (result) {
@@ -15,13 +15,13 @@ function searchMovie() {
 
                 $.each(movies, function (i, data) {
                     $('#movie-list').append(`
-                    <div class="col-md-4>
+                    <div class="col-md-4">
                         <div class="card mb-3">
                             <img src="`+ data.Poster + `" class="card-img-top" alt="...">
                             <div class="card-body">
                             <h5 class="card-title">`+ data.Title + `</h5>
                             <h6 class="card-subtitle mb-2 text-muted">`+ data.Year + `</h6>
-                            <a href="#" class="btn btn-primary see-detail" data-bs-toggle="modal" data-bs-target="#exampleModal" data-id="` + data.imdbID + `">See Detail..</a>
+                            <a href="#" class="card-link see-detail" data-bs-toggle="modal" data-bs-target="#exampleModal" data-id="` + data.imdbID + `">See Detail..</a>
                             </div>
                         </div>
                     </div>
@@ -34,7 +34,7 @@ function searchMovie() {
 
             } else {
                 $('#movie-list').html(`
-                    <div class="col>
+                    <div class="col">
                         <h1 class="text-center">` + result.Error + `</h1>
                     </div>                    
                 `)
@@ -65,7 +65,7 @@ $('#movie-list').on('click', '.see-detail', function () {
             'i': $(this).data('id')
         },
         success: function (movie) {
-            if('movie.Response === "True') {
+            if(movie.Response === "True") {
 
                 $('.modal-body').html(`
                     <div class="container-fluid">
